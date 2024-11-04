2b5e9e8052abbf3713d6fdd84e1a8425d373b32d
Faisal <faisal@owimer.co>
2024-11-04 12:44:26 -0500

    global state for taskForm component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,7 @@ import { PriorityProps, StatusProps } from "../../types/types";
 export default function Home() {
   return (
     <div>
-      <h1>Task Manager</h1>
-      <TaskForm
-        title=""
-        description=""
-        status={StatusProps.EMPTY}
-        priority={PriorityProps.EMPTY}
-        createdAt={new Date()}
-        dueDate={new Date()}
-      />
+      <TaskForm />
     </div>
   );
 }
